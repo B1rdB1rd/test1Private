97d559f35617f2837b3757e39990ee7fc9a4aacf
B1rdB1rd <ddddapeng@126.com>
2023-03-08 10:57:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ def hello(i):
     # classifier = pipeline("sentiment-analysis")
     # a = classifier(i)
     # return a
-    while True:
-        print('test')
-        time.sleep(1)
-    return "hello" + i
+#     while True:
+#         print('test')
+#         time.sleep(1)
+    return "hello"
     
 iface = gr.Interface(fn=hello, inputs="text", outputs="text")
 iface.launch(server_name="0.0.0.0")
