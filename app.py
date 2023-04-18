f68c03a1b521c46417153cb91ec2cad47d875540
B1rdB1rd <ddddapeng@126.com>
2023-04-18 11:03:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,8 @@
 import gradio as gr
-# import time
-# import os
-def hello(i):
 
-    return "Hello " + "!!"
-#     classifier = pipeline("sentiment-analysis")
-#     a = classifier(i)
-    
-#     return a
-#     while True:
-#         print('test')
-#         time.sleep(1)
-#     return "hello"
-    
-# tryghgmjhkk,:
+def greet(name):
+    return "Hello " + name + "!!"
+    # return 123/0
 
-    iface = gr.Interface(fn=hello, inputs="text", outputs="text")
-    iface.launch(server_name="0.0.0.0")
-# except:
-#     print('err')
-#     os.exit(-1)
+iface = gr.Interface(fn=greet, inputs="text", outputs="text")
+iface.launch()
