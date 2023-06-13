8725898c7f11313b55cf2d35cdc8d8eea9d0cea1
B1rdB1rd <ddddapeng@126.com>
2023-06-13 10:07:39 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+import gradio as gr
+
+def greet(name):
+    return "Hello " + name + "!!"
+    # return 123/0
+
+    
+    
+iface = gr.Interface(fn=greet, inputs="text", outputs="text")
+iface.launch()
